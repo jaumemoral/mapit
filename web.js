feb17316a69a33871b5b55d2a67949c492117dd0
jaumemoral <jaumem@fib.upc.edu>
2014-03-15 16:56:21 +0100

    Init

---
+++ web.js
@@ -0,0 +1,14 @@
+// MapIt
+
+var express = require("express");
+var app = express();
+
+app.get('/', function(req, res) {
+  res.send('Hello MapIt! Lots of things to do');
+});
+
+var port = Number(process.env.PORT || 5000);
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
+
